73f59309021640e36423f8b622adc0eb996c7e93
garden0512 <12gundam05@gmail.com>
2025-12-10 02:19:53 +0900

    Feat : 종료 버튼 기능 추가

--- FinalProject/MainWindow.java
+++ FinalProject/MainWindow.java
@@ -2,9 +2,77 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.GraphicsDevice;
 
 public class MainWindow extends JFrame{
+    private JButton exitButton;
+    private int deviceWidth;
+    private int deviceHeight;
 
+    //게임을 플레이 할 수 있도록 하는 객체 생성자 메소드
+    public MainWindow()
+    {
+        super("메인화면");      // 창 타이틀
+
+        //기본 프레임
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //프레임을 닫으면 메모리에서 제거되도록 설정하는 기능
+        this.setUndecorated(true);      // 작업줄 또는 창 테두리 등이 안보이도록 하는 기능
+        this.setLayout(null);       // 비율 기반 배치를 위한 null 레이아웃 설정
+
+        //풀스크린으로 프레임이 열리도록 설정
+        GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        if(graphicsDevice.isFullScreenSupported())      //만약 이 디바이스가 풀스크린을 지원한다면
+        {
+            graphicsDevice.setFullScreenWindow(this);   // 이 프레임이 풀스크린으로 열리도록 함
+            this.deviceWidth = graphicsDevice.getDisplayMode().getWidth();      // 현재 프레임의 가로값
+            this.deviceHeight = graphicsDevice.getDisplayMode().getHeight();    // 현재 프레임의 세로값
+        }
+        else    // 풀스크린을 지원하지 않는다면
+        {
+            System.out.println("풀스크린을 지원하지 않는 기기입니다.");     // 경고 안내문구 출력
+            this.setExtendedState(JFrame.MAXIMIZED_BOTH);       // 가로, 세로 전부 최대치로 열리도록 함
+            this.deviceWidth = graphicsDevice.getDisplayMode().getWidth();      // 현재 프레임의 가로값
+            this.deviceHeight = graphicsDevice.getDisplayMode().getHeight();    // 현재 프레임의 세로값
+        }
+
+        //버튼 생성
+        this.exitButton = CreateRelativeButton("Exit", deviceWidth, deviceHeight, 3, 88, 16, 5);
+        this.exitButton.addActionListener(new ActionListener()      // 해당 버튼을 누를 시 발생할 이벤트 설정
+        {
+           @Override
+           public void actionPerformed(ActionEvent e)
+           {
+               Exit();  // 게임 종료 메소드 호출
+           }
+        });
+        this.add(exitButton);
+
+        //가시성 설정
+        this.setVisible(true);      // 프레임이 보이도록 설정
+    }
+
+    //요소들을 화면에 맞게 동적으로 비율을 정해서 상대적으로 배치
+    private JButton CreateRelativeButton(String buttonText, int width, int height, int xPositionPercent, int yPositionPercent, int elementWidthPercent, int elementHeightPercent)
+    {
+        int positionX = (int)((width * xPositionPercent) / 100.0);       // 현재 화면을 기준으로 계산된 버튼의 상대적 가로 위치
+        int positionY = (int)((height * yPositionPercent) / 100.0);    // 현재 화면을 기준으로 계산된 버튼의 상대적 세로 위치
+        int elementWidth = (int) ((width * elementWidthPercent) / 100.0);    // 현재 화면을 기준으로 계산된 버튼의 상대적 가로값
+        int elementHeight = (int)((height * elementHeightPercent) / 100.0);    // 현재 화면을 기준으로 계산된 버튼의 상대적 세로값
+        JButton newButton = new JButton(buttonText);        // 버튼 컴포넌트 생성
+        newButton.setBounds(positionX, positionY, elementWidth, elementHeight);     // 계산된 값들을 이용해 상대위치 / 크기로 버튼 생성
+        return newButton;       // 생성된 버튼 반환
+    }
+
+    //버튼 기능 메소드들
+    private void Exit()
+    {
+        System.exit(0);
+    }
+
+    // 게임을 실행하도록 해주는 실행 메소드
+    public static void main(String[] args)
+    {
+        new MainWindow();       // 새 객체 생성
+    }
 }
