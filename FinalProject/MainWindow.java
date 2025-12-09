cf54533e63b42dbf23ecf76e3275cdd3fa07a274
Garden0512 <12gundam05@gmail.com>
2025-12-09 23:49:31 +0900

    프로젝트 생성

---
+++ FinalProject/MainWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+public class MainWindow extends JFrame{
+
+}
