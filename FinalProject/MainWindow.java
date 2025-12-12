b700f446297d4aed7ca89e2be59ea3a0311a534e
garden0512 <12gundam05@gmail.com>
2025-12-13 03:15:40 +0900

    Feat : 스팀 페이지 열기 버튼 UI 완성

--- FinalProject/MainWindow.java
+++ FinalProject/MainWindow.java
@@ -25,6 +25,7 @@ public class MainWindow extends JFrame{
     ImageIcon loadGameDefaultIcon = new ImageIcon("images/load_default.png");
     ImageIcon settingDefaultIcon = new ImageIcon("images/setting_default.png");
     ImageIcon studioNewsDefaultIcon = new ImageIcon("images/studio_default.png");
+    ImageIcon steamPageDefaultIcon = new ImageIcon("images/steam_default.png");
 
     //커서 호버링 이미지 로딩
     ImageIcon continueRollOverIcon = new ImageIcon("images/continue_hover.png");
@@ -33,6 +34,7 @@ public class MainWindow extends JFrame{
     ImageIcon loadGameRollOverIcon = new ImageIcon("images/load_hover.png");
     ImageIcon settingRollOverIcon = new ImageIcon("images/setting_hover.png");
     ImageIcon studioNewsRollOverIcon = new ImageIcon("images/studio_hover.png");
+    ImageIcon steamPageRollOverIcon = new ImageIcon("images/steam_hover.png");
 
     //클릭 이미지 로딩
     ImageIcon continuePressedIcon = new ImageIcon("images/continue_hover.png");
@@ -41,6 +43,7 @@ public class MainWindow extends JFrame{
     ImageIcon loadGamePressedIcon = new ImageIcon("images/load_hover.png");
     ImageIcon settingPressedIcon = new ImageIcon("images/setting_hover.png");
     ImageIcon studioNewsPressedIcon = new ImageIcon("images/studio_hover.png");
+    ImageIcon steamPagePressedIcon = new ImageIcon("images/steam_hover.png");
 
     //게임을 플레이 할 수 있도록 하는 객체 생성자 메소드
     public MainWindow()
@@ -81,7 +84,7 @@ public class MainWindow extends JFrame{
         //버튼 생성
         this.exitButton = CreateRelativeButton(continueDefaultIcon, deviceWidth, deviceHeight, 15, 88, 16, 4);
         this.productionListButton = CreateRelativeButton(continueDefaultIcon, deviceWidth, deviceHeight, 15, 82, 16, 4);
-        this.steamPageButton = CreateRelativeButton(continueDefaultIcon, deviceWidth, deviceHeight, 15, 76, 16, 4);
+        this.steamPageButton = CreateRelativeButton(steamPageDefaultIcon, deviceWidth, deviceHeight, 15, 76, 16, 4);
         this.studioNewsButton = CreateRelativeButton(studioNewsDefaultIcon, deviceWidth, deviceHeight, 15, 70, 16, 4);
         this.settingButton = CreateRelativeButton(settingDefaultIcon, deviceWidth, deviceHeight, 15, 54, 16, 4);
         this.loadGameButton = CreateRelativeButton(loadGameDefaultIcon, deviceWidth, deviceHeight, 15, 48, 16, 4);
@@ -203,6 +206,7 @@ public class MainWindow extends JFrame{
         loadGameButton.setRolloverIcon(loadGameRollOverIcon);
         settingButton.setRolloverIcon(settingRollOverIcon);
         studioNewsButton.setRolloverIcon(studioNewsRollOverIcon);
+        steamPageButton.setRolloverIcon(steamPageRollOverIcon);
 
         //버튼 클릭 상태 설정
         continueGameButton.setPressedIcon(continuePressedIcon);
@@ -211,6 +215,7 @@ public class MainWindow extends JFrame{
         loadGameButton.setPressedIcon(loadGamePressedIcon);
         settingButton.setPressedIcon(settingPressedIcon);
         studioNewsButton.setPressedIcon(studioNewsPressedIcon);
+        steamPageButton.setPressedIcon(steamPagePressedIcon);
     }
 
     //요소들을 화면에 맞게 동적으로 비율을 정해서 상대적으로 배치
